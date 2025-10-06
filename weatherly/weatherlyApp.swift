6fa212b36d1f813010f8e5800dad202d4cee091d
noureddine-louafi-nw <noureddine.louafi@nimbleways.com>
2025-10-06 19:44:39 +0100

    Initial Commit

---
+++ weatherly/weatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherlyApp.swift
+//  weatherly
+//
+//  Created by noureddine on 6/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherlyTests/weatherlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherlyTests.swift
+//  weatherlyTests
+//
+//  Created by noureddine on 6/10/2025.
+//
+
+import Testing
+@testable import weatherly
+
+struct weatherlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
